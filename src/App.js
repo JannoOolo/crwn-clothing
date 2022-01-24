06427b05bc2f6d32b5599b4beba44dae7553e853
Janno Oolo <janno.oolo@nixor.eu>
2022-01-25 00:49:41 +0200

    added header component + styles

--- src/App.js
+++ src/App.js
@@ -2,10 +2,12 @@ import "./App.css";
 import HomePage from "./pages/homepage/homepage.component";
 import { Route } from "react-router-dom";
 import ShopPage from "./pages/shop/shop.component";
+import Header from "./components/header/header.component.jsx";
 
 function App() {
   return (
     <div>
+      <Header />
       <Route exact path="/" component={HomePage} />
       <Route path="/shop" component={ShopPage} />
     </div>
